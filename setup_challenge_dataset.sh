603d27c21617be2d2ec1df60b7842683dca4e3b2
dsseo <sorld0603@snu.ac.kr>
2023-06-21 03:07:04 +0900

    update challenge dataset script

---
+++ setup_challenge_dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DATA_ROOT=/workspace/view-synthesis-challenge/data/devPhase
+
+gdown https://drive.google.com/uc?id=1qhe2GTjBuTmRkco2SFrwjVMa0Nho2nYx -O $DATA_ROOT/devPhase_01.zip # devPhase_01.zip
+gdown https://drive.google.com/uc?id=1l1LEry5y7fn3Zpj_BjxvxWU6IV1H-Ey7 -O $DATA_ROOT/devPhase_02.zip # devPhase_02.zip
+
+unzip $DATA_ROOT/devPhase_01.zip -d $DATA_ROOT
+unzip $DATA_ROOT/devPhase_02.zip -d $DATA_ROOT
+
+rm -rf $DATA_ROOT/devPhase_01.zip $DATA_ROOT/devPhase_02.zip
