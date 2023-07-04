31dff0ded2c0fc6ea111a679173a8a7683c4f210
solanian <sorld0603@snu.ac.kr>
2023-07-04 14:04:39 +0000

    fix script

--- setup_challenge_dataset.sh
+++ setup_challenge_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATA_ROOT=/workspace/view-synthesis-challenge/data_root/devPhase
+DATA_ROOT=data_root/devPhase
 
 gdown 1wUztXKTFmP4g-yOuv3MuRBijz1S25r48 -O $DATA_ROOT/devPhase_01.zip # devPhase_01.zip
 gdown 17aUOFbvci5Cd9_OUe6rpWp0vYRxPRvjJ -O $DATA_ROOT/devPhase_02.zip # devPhase_02.zip
