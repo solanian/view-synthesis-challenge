d6a9aaea7804129765a0658a1f008b76bea6e72e
dsseo <sorld0603@snu.ac.kr>
2023-07-13 21:02:58 +0900

    update challenge dataset

---
+++ setup_development_dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DATA_ROOT=data_root/devPhase
+
+gdown 1wUztXKTFmP4g-yOuv3MuRBijz1S25r48 -O $DATA_ROOT/devPhase_01.zip # devPhase_01.zip
+gdown 17aUOFbvci5Cd9_OUe6rpWp0vYRxPRvjJ -O $DATA_ROOT/devPhase_02.zip # devPhase_02.zip
+
+unzip $DATA_ROOT/devPhase_01.zip -d $DATA_ROOT
+unzip $DATA_ROOT/devPhase_02.zip -d $DATA_ROOT
+
+rm -rf $DATA_ROOT/devPhase_01.zip $DATA_ROOT/devPhase_02.zip
