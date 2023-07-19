de059f3e7ed2a2f317f5f4691b2d1dbe7672d6c9
dsseo <sorld0603@snu.ac.kr>
2023-07-19 20:23:15 +0900

    update setup script

--- setup_challenge_dataset.sh
+++ setup_challenge_dataset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DATA_ROOT=data_root/devPhase
+DATA_ROOT=data_root/chaPhase
 
-gdown 1wUztXKTFmP4g-yOuv3MuRBijz1S25r48 -O $DATA_ROOT/devPhase_01.zip # devPhase_01.zip
-gdown 17aUOFbvci5Cd9_OUe6rpWp0vYRxPRvjJ -O $DATA_ROOT/devPhase_02.zip # devPhase_02.zip
+gdown 1iRXdxKNfVpGCUbKtVNbrr95i8L_RdICf -O $DATA_ROOT/chaPhase_01.zip # chaPhase_01.zip
+gdown 1IU9gyO-PFo8eODh_TNGJIB3WpMWq2KCt -O $DATA_ROOT/chaPhase_02.zip # chaPhase_02.zip
 
-unzip $DATA_ROOT/devPhase_01.zip -d $DATA_ROOT
-unzip $DATA_ROOT/devPhase_02.zip -d $DATA_ROOT
+unzip $DATA_ROOT/chaPhase_01.zip -d $DATA_ROOT
+unzip $DATA_ROOT/chaPhase_02.zip -d $DATA_ROOT
 
-rm -rf $DATA_ROOT/devPhase_01.zip $DATA_ROOT/devPhase_02.zip
+rm -rf $DATA_ROOT/chaPhase_01.zip $DATA_ROOT/chaPhase_02.zip
