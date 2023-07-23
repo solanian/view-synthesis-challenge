c4f5eff36816a520ba245fa64a165c5394729fd1
ylab604 <99255566+ylab604@users.noreply.github.com>
2023-07-24 01:02:51 +0900

    Update setup_requirment.sh

--- setup_requirment.sh
+++ setup_requirment.sh
@@ -4,4 +4,4 @@ pip3 install torchmetrics==0.7
 pip install tqdm scikit-image opencv-python configargparse lpips imageio-ffmpeg kornia lpips tensorboard
 pip install plyfile
 pip install ninja
-pip install ./gridencoder
+
